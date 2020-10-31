184892156a8ebc06e9fab57a3048269a8aead1d7
Llm Dl <LlmDlio@gmail.com>
2020-10-30 19:08:28 -0500

    War Event Fun Times Commit.
    - Fixes up scoring to show different depending on warType.
    - Deprecate some completely unused things.
    - Make end determined by warType.
    - Add warriorPlayer list.
    - Use warriorPlayer list to make it so 1/2 of the tests in WarTimer task
    are not needed any longer.
    - Add tab-complete to /ta war command (because once you've had tab
    complete it is painful to live without even when it's just debugging.)

--- src/com/palmergames/bukkit/towny/TownyUniverse.java
+++ src/com/palmergames/bukkit/towny/TownyUniverse.java
@@ -779,7 +779,7 @@ public class TownyUniverse {
     
     public void endWarEvent() {
         if (warEvent != null && warEvent.isWarTime()) {
-            warEvent.toggleEnd();
+            warEvent.end();
         }
     }	
 
@@ -844,7 +844,13 @@ public class TownyUniverse {
     	return false;
     }
     
-    
+    public boolean hasWarEvent(Resident resident) {
+     	for (War war : getWars()) {
+     		if (war.isWarringResident(resident))
+				return true;
+     	}
+     	return false;
+    }
     public void setWarEvent(War warEvent) {
         this.warEvent = warEvent;
     }
